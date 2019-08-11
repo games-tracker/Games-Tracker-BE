6aa19e7a48dfb326989ff78ce636bf125644aeb6
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-11 12:22:36 -0400

    Added CORS Error Handling

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Handle CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // Profile routes
 app.use("/api/v1/profile", require("./routes/profile"));
 
