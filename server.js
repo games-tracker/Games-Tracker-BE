aed92030b8629454036ba3bf5a42fb38a49e91f4
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-12 18:59:32 -0400

    Removed use of overwatchProfile

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 // Profile routes
 app.use("/api/v1/profile", require("./routes/apexProfile"));
-app.use("/api/v1/profile", require("./routes/overwatchProfile"));
+// app.use("/api/v1/profile", require("./routes/overwatchProfile"));
 
 const port = process.env.PORT || 8000;
 
